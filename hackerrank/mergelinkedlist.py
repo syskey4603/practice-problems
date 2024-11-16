5420f9c62d66a060e8f78ffa00905785f9a50346
sxskey <syskeyownsyou@gmail.com>
2024-11-16 18:44:31 +0530

    Update mergelinkedlist.py

--- hackerrank/mergelinkedlist.py
+++ hackerrank/mergelinkedlist.py
@@ -24,20 +24,52 @@ class SinglyLinkedList:
 def comparelist(llist1, llist2):
     list1 = llist1
     list2 = llist2
-
+    prev = None
     while list1:
-        if list1.data > list2.data:
-            pass
+        if list1.data >= list2.data:
 
-        list1 = list1.next
-        list2 = list2.next
+            temp = list2
+            list2 = list2.next
+            if not prev:
+                t = llist1
+                llist1 = temp
+                list1 = temp
+                temp.next = t
+            else:
+                prev.next = temp
+                prev.next.next = list1
+                prev = prev.next
+            if not list2:
+                return llist1
+        else:
+            prev = list1
+            list1 = list1.next
+
+    prev.next = list2
+    return llist1
+
+def printlist(head):
+    cur = head
+    while cur:
+        print(cur.data)
+        cur = cur.next
+
+
 
 
 llist1 = SinglyLinkedList()
-llist1.insert_node(1)
-llist1.insert_node(2)
+llist1.insert_node(100)
+
 
 llist2 = SinglyLinkedList()
-llist2.insert_node(1)
-llist2.insert_node(3)
-print(comparelist(llist1.head, llist2.head))
+
+llist2.insert_node(8)
+llist2.insert_node(11)
+llist2.insert_node(17)
+llist2.insert_node(20)
+llist2.insert_node(20)
+llist2.insert_node(42)
+llist2.insert_node(83)
+llist2.insert_node(94)
+llist2.insert_node(95)
+printlist(comparelist(llist1.head, llist2.head))
