469cf80dbc9f17ef6efa69eda0294f85132c93a9
sxskey <syskeyownsyou@gmail.com>
2024-12-01 07:31:50 +0530

    Create marsexplore.py

---
+++ hackerrank/marsexplore.py
@@ -0,0 +1,31 @@
+def marsexplore(s):
+    finalnum = 0
+    count = 1
+    for x in s:
+        if x != "O" and x != "S":
+            finalnum += 1
+            count+=1
+            continue
+        if count == 1:
+            if x != "S":
+                finalnum+=1
+        elif count == 2:
+            if x != "O":
+                finalnum+=1
+        elif count == 3:
+            if x != "S":
+                finalnum+=1
+            count = 1
+            continue
+        count += 1
+
+    return finalnum
+
+        
+
+
+
+
+
+s = "OOSDSSOSOSWEWSOSOSOSOSOSOSSSSOSOSOSS"
+print(marsexplore(s))
