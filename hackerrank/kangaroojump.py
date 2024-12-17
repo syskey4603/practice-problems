57fef1ccc520d384e67a0086899499d696720983
sxskey <syskeyownsyou@gmail.com>
2024-12-17 18:55:29 +0530

    Update kangaroojump.py

--- hackerrank/kangaroojump.py
+++ hackerrank/kangaroojump.py
@@ -1,8 +1,11 @@
 def kangaroo(x1, v1, x2, v2):
     if x1 < x2 and v1 > v2:
-        print("YES")
+        if abs(v1-v2) > abs(x1-x2):
+            print("NO")
+        
     elif x1 > x2 and v1 < v2:
-        print("YES")
+        if abs(v1-v2) > abs(x1-x2):
+            print("NO")
     else:
         print("NO")
     
@@ -19,6 +22,8 @@ print(kangaroo(x1,v1,x2,v2))
 '''
 
 if the first one starts earlier but has a larger jump its possible
+
+if the diff between startings is greater 
 
 
 '''
