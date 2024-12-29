5931c811a623484783cd4e504ea60e485c5ef046
sxskey <syskeyownsyou@gmail.com>
2024-12-29 09:39:47 +0530

    Update pickingnums.py

--- hackerrank/pickingnums.py
+++ hackerrank/pickingnums.py
@@ -1,10 +1,36 @@
-def pickingnums(a):
+def eachelement(a, el):
     a.sort()
     newarr = []
-    for x in range(len(a)-1):
-        if a[x] == a[x+1] or a[x]+1==a[x+1]:
+    for x in range(el, len(a)):
+        if not newarr or newarr[0] == a[x] or abs(newarr[0]-a[x]) == 1:
             newarr.append(a[x])
     return newarr
 
-a = [4, 6, 5, 3, 3, 1]
+def pickingnums(a):
+    returnlist = []
+    curvals = []
+    largelen = 0
+    for x in range(len(a)):
+        curvals = eachelement(a, x)
+        if len(curvals) > largelen:
+            largelen = len(curvals)
+            returnlist = curvals
+    return len(returnlist)
+
+
+
+
+a = [
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66
+]
+
 print(pickingnums(a))
