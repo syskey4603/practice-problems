3247ef2996bcd6375ec6b21676608fd25e6f4e1a
sxskey <syskeyownsyou@gmail.com>
2025-01-20 05:34:58 +0530

    Create linkedlistprac.py

---
+++ hackerrank/linkedlistprac.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_node(self, data):
+        newnode = Node(data)
+        if not self.head:
+            self.head = newnode
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = newnode
+
+    def printlist(self):
+        cur = self.head
+        while cur:
+            print(cur.data)
+            cur = cur.next
+
+linkedlist = SinglyLinkedList()
+linkedlist.insert_node(4)
+linkedlist.insert_node(3)
+linkedlist.insert_node(2)
+linkedlist.insert_node(1)
+
+linkedlist.printlist()
