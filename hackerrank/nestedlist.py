f4cf99d5f212143f5b0b4fc0fcc5b99779dcced0
sxskey <syskeyownsyou@gmail.com>
2025-02-13 13:51:32 +0530

    Create nestedlist.py

---
+++ hackerrank/nestedlist.py
@@ -0,0 +1,27 @@
+nestedlist = []
+for _ in range(int(input())):
+    templist=[]
+    name = input()
+    score = float(input())
+    templist.append(name)
+    templist.append(score)
+    nestedlist.append(templist)
+lowest = 10000000
+secondlowest = 10000000
+for x in nestedlist:
+    if x[1] < lowest:
+        lowest = x[1]
+for y in nestedlist:
+    if y[1] == lowest:
+        continue
+    else:
+        if y[1] < secondlowest:
+            secondlowest = y[1]
+returnlist = []
+for z in nestedlist:
+    if z[1] == secondlowest:
+        returnlist.append(z[0])
+returnlist.sort()
+for i in returnlist:
+    print(i)
+        
